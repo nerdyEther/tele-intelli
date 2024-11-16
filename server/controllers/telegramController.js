7c58bc8c12a2dc6aefef3a31aaae78215345ccff
nerdyEther <ether1926@gmail.com>
2024-11-16 14:26:04 +0530

    Removed server from repo

--- server/controllers/telegramController.js
+++
@@ -1,8 +0,0 @@
-const handleIncomingMessage = (req, res) => {
-    const message = req.body.message;
-    // Process the message, analyze keywords, sentiment, etc.
-    res.status(200).json({ success: true, message: 'Message processed' });
-  };
-  
-  module.exports = { handleIncomingMessage };
-  
--- server/routes/telegram.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleIncomingMessage } = require('../controllers/telegramController');
-
-router.post('/message', handleIncomingMessage);
-
-module.exports = router;
